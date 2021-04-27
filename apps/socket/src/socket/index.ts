96f45ada1521d416063ee75de55b41cbbc6146b9
PLHB1712289 <phanlehoaibaok10@gmail.com>
2021-04-27 20:59:44 +0700

    authenticate socket

--- apps/socket/src/socket/index.ts
+++ apps/socket/src/socket/index.ts
@@ -1,8 +1,18 @@
 import socketIO from "socket.io";
-import socketioJwt from "socketio-jwt";
+import _ from "underscore";
 import configApp from "../config";
 import configEventListener from "./eventListener";
 import { TAG_LOG, TAG_LOG_ERROR } from "./TAG_EVENT";
+import jwt from "jsonwebtoken";
+
+const checkAuthToken = (token, callback) => {
+  try {
+    const decode = jwt.verify(token, configApp.JWT.secretKey);
+    callback(null, decode);
+  } catch (e) {
+    callback(e, null);
+  }
+};
 
 // Save io
 let _io = null;
@@ -15,34 +25,53 @@ export const config = (server) => {
   // @ts-expect-error
   _io = socketIO(server, {
     cors: true,
-    origins: [`${configApp.URL_SERVER}:${process.env.port}`],
+    origins: [`${configApp.URL_SERVER}:${configApp.PORT}`],
   });
 
   // @ts-expect-error
   _io.on("connection", (socket) => {
-    console.log(`[${TAG_LOG}]: new connection ${socket.id}`);
-    console.log(`[${TAG_LOG}]: token ${JSON.stringify(socket.decoded_token)}`);
+    socket.auth = false;
+    socket.on("authenticate", (data) => {
+      checkAuthToken(data.token, (err, success) => {
+        if (!err && success) {
+          console.log(`[${TAG_LOG}]: Authenticated socket ${socket.id}`);
+          socket.auth = true;
+          socket.decode = success;
+          console.log(socket.decode);
 
-    socket.emit("authenticated");
+          // @ts-expect-error
+          _.each(_io.nsps, (nsp) => {
+            if (_.findWhere(nsp.sockets, { id: socket.id })) {
+              console.log(`[${TAG_LOG}]: Restoring socket to ${nsp.name}`);
+              nsp.connected[socket.id] = socket;
+            }
+          });
 
-    // Set listener
-    configEventListener(_io, socket);
-
-    // disconnect
-    socket.on("disconnect", async () => {
-      console.log(`[${TAG_LOG}]: disconnect ${socket.id}`);
+          socket.emit("authenticated", "Authenticated");
+        } else {
+          socket.emit("unauthenticate", err.message);
+        }
+      });
     });
+
+    setTimeout(() => {
+      //sau 1s mà client vẫn chưa dc auth, lúc đấy chúng ta mới disconnect.
+      if (!socket.auth) {
+        console.log(`[${TAG_LOG}]: Disconnecting socket ${socket.id}`);
+        socket.disconnect("unauthorized");
+      }
+    }, 1000);
   });
 
   // @ts-expect-error
-  _io.use(
-    socketioJwt.authorize({
-      secret: configApp.JWT.secretKey,
-      handshake: true,
-      auth_header_required: true,
-      timeout: 5000,
-    })
-  );
+  _.each(_io.nsps, (nsp) => {
+    nsp.on("connect", (socket) => {
+      if (!socket.auth) {
+        console.log(`[${TAG_LOG}]: Removing socket from ${nsp.name}`);
+        delete nsp.connected[socket.id];
+      }
+    });
+  });
 
   console.log(`[${TAG_LOG}]: Config socket.io success`);
 };
