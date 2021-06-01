9240737a9d785d214d653eb11b53f5e031de62e5
PLHB1712289 <phanlehoaibaok10@gmail.com>
2021-06-01 10:36:09 +0700

    refactor ggapi module

--- apps/client/src/googleAPI/index.ts
+++ apps/client/src/googleAPI/index.ts
@@ -1,146 +1,171 @@
 import { google } from "googleapis";
 import fs from "fs";
 import readline from "readline";
+import path from "path";
 
-import credentials from "./credentials";
+class GGAPI {
+  private ACCOUNT_GG: { email: string; pass: string } = {
+    email: "flashfood1234@gmail.com",
+    pass: "Fl@shfood1234",
+  };
+  private TOKEN_PATH = "./token.json";
 
-const oAuth2Client: any = new google.auth.OAuth2(
-  credentials.CLIENT_ID,
-  credentials.CLIENT_SECRET,
-  credentials.REDIRECT_URLS[0]
-);
+  private credentials: any = null;
+  private oAuth2Client: any = null;
+  private drive: any = null;
 
-const TOKEN_PATH = "";
+  constructor() {
+    const pathCredentials = path.join(
+      __dirname.replace("/dist", "").replace("\\dist", ""),
+      "src",
+      "googleAPI",
+      "credentials.json"
+    );
 
-oAuth2Client.setCredentials({ refresh_token: credentials.REFRESH_TOKEN });
-const drive = google.drive({ version: "v3", auth: oAuth2Client });
+    this.credentials = JSON.parse(fs.readFileSync(pathCredentials).toString());
 
-function getAccessToken() {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: "offline",
-    scope: credentials.SCOPES,
-  });
+    this.oAuth2Client = new google.auth.OAuth2(
+      this.credentials.CLIENT_ID,
+      this.credentials.CLIENT_SECRET,
+      this.credentials.REDIRECT_URLS[0]
+    );
 
-  console.log("Authorize this app by visiting this url:", authUrl);
+    this.oAuth2Client.setCredentials({
+      refresh_token: this.credentials.REFRESH_TOKEN,
+    });
+    this.drive = google.drive({ version: "v3", auth: this.oAuth2Client });
+  }
 
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+  private getAccessToken() {
+    const authUrl = this.oAuth2Client.generateAuthUrl({
+      access_type: "offline",
+      scope: this.credentials.SCOPES,
+    });
 
-  rl.question("Enter the code from that page here: ", (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error("Error retrieving access token", err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) return console.error(err);
-        console.log("Token stored to", TOKEN_PATH);
+    console.log("Authorize this app by visiting this url:", authUrl);
+
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    rl.question("Enter the code from that page here: ", (code) => {
+      rl.close();
+      this.oAuth2Client.getToken(code, (err, token) => {
+        if (err) return console.error("Error retrieving access token", err);
+        this.oAuth2Client.setCredentials(token);
+        // Store the token to disk for later program executions
+        fs.writeFile(this.TOKEN_PATH, JSON.stringify(token), (err) => {
+          if (err) return console.error(err);
+          console.log("Token stored to", this.TOKEN_PATH);
+        });
       });
     });
-  });
-}
+  }
 
-// const listFiles = async () => {
-//   drive.files.list(
-//     {
-//       pageSize: 10,
-//       fields: "nextPageToken, files(id, name)",
-//     },
-//     (err, res) => {
-//       if (err) return console.log("The API returned an error: " + err);
-//       const files = res.data.files;
-//       if (files.length) {
-//         console.log("Files:");
-//         files.map((file) => {
-//           console.log(file);
-//           // console.log(`${file.name} (${file.id})`);
-//         });
-//       } else {
-//         console.log("No files found.");
-//       }
-//     }
-//   );
-// };
+  private async generatePublicUrl(
+    fileId: string | null
+  ): Promise<{ webContentLink: string | null; webViewLink: string | null }> {
+    try {
+      await this.drive.permissions.create({
+        fileId: fileId,
+        requestBody: {
+          role: "reader",
+          type: "anyone",
+        },
+      });
 
-async function generatePublicUrl(fileId) {
-  try {
-    await drive.permissions.create({
-      fileId: fileId,
-      requestBody: {
-        role: "reader",
-        type: "anyone",
-      },
-    });
+      /* 
+      webViewLink: View the file in browser
+      webContentLink: Direct download link 
+      */
+      const result: any = await this.drive.files.get({
+        fileId: fileId,
+        fields: "webViewLink, webContentLink",
+      });
+      return result.data;
+    } catch (error) {
+      console.log(`[UPLOAD_GENERATE_PUBLIC_URL_FAILED]: ${error.message}`);
+      return { webContentLink: null, webViewLink: null };
+    }
+  }
 
-    /* 
-    webViewLink: View the file in browser
-    webContentLink: Direct download link 
-    */
-    const result = await drive.files.get({
-      fileId: fileId,
-      fields: "webViewLink, webContentLink",
-    });
-    return result.data;
-  } catch (error) {
-    console.log(`[UPLOAD_GENERATE_PUBLIC_URL_FAILED]: ${error.message}`);
-    return { webContentLink: null, webViewLink: null };
+  async uploadFile(file: {
+    name: string;
+    type: string;
+    path: string;
+  }): Promise<{ webContentLink: string | null; webViewLink: string | null }> {
+    try {
+      const response: any = await this.drive.files.create({
+        requestBody: {
+          name: file.name, //This can be name of your choice
+          mimeType: file.type,
+        },
+        media: {
+          mimeType: file.type,
+          body: fs.createReadStream(file.path),
+        },
+      });
+
+      const link = await this.generatePublicUrl(response.data.id);
+      console.log(`[UPLOAD]:`, link);
+
+      return link;
+    } catch (error) {
+      console.log(`[UPLOAD_FAILED]: ${error.message}`);
+
+      const TOKEN_EXPIRED = "invalid_grant";
+      if (error.message === TOKEN_EXPIRED) {
+        console.log(
+          "[GGAPI_FAILED]: Invalid token, please try get new REFRESH TOKEN"
+        );
+
+        console.log(
+          "[GGAPI_FAILED]: Use this account for get new token:",
+          this.ACCOUNT_GG
+        );
+        this.getAccessToken();
+      }
+
+      return { webContentLink: null, webViewLink: null };
+    }
+  }
+
+  async deleteFile(url: string | null): Promise<boolean> {
+    if (!url) return false;
+
+    let fileId: string | null = null;
+
+    const LINK1 = {
+      head: "https://drive.google.com/uc?id=",
+      tail: "&export=download",
+    };
+
+    const LINK2 = {
+      head: "https://drive.google.com/file/d/",
+      tail: "/view?usp=",
+    };
+
+    if (url.indexOf(LINK1.head) > -1) {
+      fileId = url.slice(LINK1.head.length, url.indexOf(LINK1.tail));
+    } else if (url.indexOf(LINK2.head) > -1) {
+      fileId = url.slice(LINK2.head.length, url.indexOf(LINK2.tail));
+    }
+
+    try {
+      const response = await this.drive.files.delete({
+        fileId,
+      });
+      console.log(
+        `[DELETE]: success delete file with id ${fileId}, status ${response.status}`
+      );
+      return true;
+    } catch (error) {
+      console.log(`[DELETE_FAILED]: ${error.message}`);
+      return false;
+    }
   }
 }
 
-export const uploadFile = async (file) => {
-  try {
-    const response = await drive.files.create({
-      requestBody: {
-        name: file.name, //This can be name of your choice
-        mimeType: file.type,
-      },
-      media: {
-        mimeType: file.type,
-        body: fs.createReadStream(file.path),
-      },
-    });
-
-    console.log(response.data);
-
-    return await generatePublicUrl(response.data.id);
-  } catch (error) {
-    console.log(`[UPLOAD_FAILED]: ${error.message}`);
-    return { webContentLink: null, webViewLink: null };
-  }
-};
-
-export const deleteFile = async (url) => {
-  let fileId = null;
-
-  const LINK1 = {
-    head: "https://drive.google.com/uc?id=",
-    tail: "&export=download",
-  };
-
-  const LINK2 = {
-    head: "https://drive.google.com/file/d/",
-    tail: "/view?usp=",
-  };
-
-  if (url.indexOf(LINK1.head) > -1) {
-    fileId = url.slice(LINK1.head.length, url.indexOf(LINK1.tail));
-  } else if (url.indexOf(LINK2.head) > -1) {
-    fileId = url.slice(LINK2.head.length, url.indexOf(LINK2.tail));
-  }
-
-  try {
-    const response = await drive.files.delete({
-      // @ts-expect-error
-      fileId,
-    });
-    console.log(
-      `[DELETE]: success delete file with id ${fileId}, status ${response.status}`
-    );
-    return true;
-  } catch (error) {
-    console.log(`[DELETE_FAILED]: ${error.message}`);
-    return false;
-  }
-};
+const ggAPI = new GGAPI();
+export default ggAPI;
