258f1f97ee06aa3b907c22e1babff83b7f05ca23
PLHB1712289 <phanlehoaibaok10@gmail.com>
2021-06-04 22:45:47 +0700

    calc distance using google direction api

--- libs/google-api/src/lib/google-api.ts
+++ libs/google-api/src/lib/google-api.ts
@@ -2,6 +2,12 @@ import { google } from "googleapis";
 import fs from "fs";
 import readline from "readline";
 import path from "path";
+import axios from "axios";
+
+interface Coordinate {
+  lat: number;
+  lng: number;
+}
 
 class GGAPI {
   private ACCOUNT_GG: { email: string; pass: string } = {
@@ -13,6 +19,12 @@ class GGAPI {
   private credentials: any = null;
   private oAuth2Client: any = null;
   private drive: any = null;
+
+  private _TAG_LOG = "[GG_API]";
+  private _TAG_LOG_FAIL = "[GG_API_FAIL]";
+
+  private _TEMPLATE_GOOGLE_DIRECT =
+    "https://maps.googleapis.com/maps/api/directions/json?origin=__ORIGIN&destination=__DESTINATION&key=__KEY";
 
   constructor() {
     const pathCredentials = path.join(
@@ -44,7 +56,9 @@ class GGAPI {
       scope: this.credentials.SCOPES,
     });
 
-    console.log("Authorize this app by visiting this url:", authUrl);
+    console.log(
+      `${this._TAG_LOG}: Authorize this app by visiting this url: ${authUrl}`
+    );
 
     const rl = readline.createInterface({
       input: process.stdin,
@@ -59,7 +73,7 @@ class GGAPI {
         // Store the token to disk for later program executions
         fs.writeFile(this.TOKEN_PATH, JSON.stringify(token), (err) => {
           if (err) return console.error(err);
-          console.log("Token stored to", this.TOKEN_PATH);
+          console.log(`${this._TAG_LOG}: Token stored to ${this.TOKEN_PATH}`);
         });
       });
     });
@@ -85,14 +99,100 @@ class GGAPI {
         fileId: fileId,
         fields: "webViewLink, webContentLink",
       });
+
+      console.log(`${this._TAG_LOG}: generate public url success.`);
+
       return result.data;
     } catch (error) {
-      console.log(`[UPLOAD_GENERATE_PUBLIC_URL_FAILED]: ${error.message}`);
+      console.log(
+        `${this._TAG_LOG_FAIL}: generate public url fail, ${error.message}`
+      );
       return { webContentLink: null, webViewLink: null };
     }
   }
 
-  async uploadFile(file: {
+  public async calcDistance(
+    origin: string,
+    destination: string
+  ): Promise<{
+    distance: number;
+    distanceUnit: "m";
+    duration: number;
+    durationUnit: "s";
+    originAddress: string;
+    destinationAddress: string;
+    originCoor: Coordinate;
+    destinationCoor: Coordinate;
+  }> {
+    try {
+      const _origin = origin.split(" ").join("%20");
+      const _destination = destination.split(" ").join("%20");
+
+      // Key TEST
+      const LIST_API_KEY = [
+        "AIzaSyCgcFxR44eAe0y6tdjymkqRfgUtXN2ceSI",
+        this.credentials.API_KEY,
+        "AIzaSyBZqajeEV1bxzly92dXxkSy5vxf30ASsRk",
+        "AIzaSyBL5NFRIKsim5nZLWf34oVPVw-0Bl_qv-8",
+      ];
+      const API_KEY = LIST_API_KEY[0];
+
+      const query = unescape(
+        encodeURIComponent(
+          this._TEMPLATE_GOOGLE_DIRECT
+            .replace("__ORIGIN", _origin)
+            .replace("__DESTINATION", _destination)
+            .replace("__KEY", API_KEY)
+        )
+      );
+
+      const { data } = await axios.get(`${query}`);
+
+      if (data.status !== "OK") {
+        console.log(`${this._TAG_LOG_FAIL}: call google api failed.`);
+        console.log(`${this._TAG_LOG_FAIL}: ${data.error_message}.`);
+        return {
+          distance: 0,
+          distanceUnit: "m",
+          duration: 0,
+          durationUnit: "s",
+          originAddress: "",
+          destinationAddress: "",
+          originCoor: { lat: 0, lng: 0 },
+          destinationCoor: { lat: 0, lng: 0 },
+        };
+      }
+
+      const legs = data.routes[0].legs[0];
+
+      console.log(`${this._TAG_LOG}: calc distance success.`);
+
+      return {
+        distance: legs.distance.value,
+        distanceUnit: "m",
+        duration: legs.duration.value,
+        durationUnit: "s",
+        originAddress: legs.start_address,
+        destinationAddress: legs.end_address,
+        originCoor: legs.start_location,
+        destinationCoor: legs.end_location,
+      };
+    } catch (e) {
+      console.log(`${this._TAG_LOG_FAIL}: ${e.message}`);
+      return {
+        distance: 0,
+        distanceUnit: "m",
+        duration: 0,
+        durationUnit: "s",
+        originAddress: "",
+        destinationAddress: "",
+        originCoor: { lat: 0, lng: 0 },
+        destinationCoor: { lat: 0, lng: 0 },
+      };
+    }
+  }
+
+  public async uploadFile(file: {
     name: string;
     type: string;
     path: string;
@@ -110,21 +210,20 @@ class GGAPI {
       });
 
       const link = await this.generatePublicUrl(response.data.id);
-      console.log(`[UPLOAD]:`, link);
+      console.log(`${this._TAG_LOG}: upload file success, ${link}`);
 
       return link;
     } catch (error) {
-      console.log(`[UPLOAD_FAILED]: ${error.message}`);
+      console.log(`${this._TAG_LOG_FAIL}: upload file fail, ${error.message}`);
 
       const TOKEN_EXPIRED = "invalid_grant";
       if (error.message === TOKEN_EXPIRED) {
         console.log(
-          "[GGAPI_FAILED]: Invalid token, please try get new REFRESH TOKEN"
+          `${this._TAG_LOG_FAIL}: Invalid token, please try get new REFRESH TOKEN`
         );
 
         console.log(
-          "[GGAPI_FAILED]: Use this account for get new token:",
-          this.ACCOUNT_GG
+          `${this._TAG_LOG_FAIL}: Use this account for get new token: ${this.ACCOUNT_GG}`
         );
         this.getAccessToken();
       }
@@ -133,7 +232,7 @@ class GGAPI {
     }
   }
 
-  async deleteFile(url: string | null): Promise<boolean> {
+  public async deleteFile(url: string | null): Promise<boolean> {
     if (!url) return false;
 
     let fileId: string | null = null;
@@ -159,11 +258,11 @@ class GGAPI {
         fileId,
       });
       console.log(
-        `[DELETE]: success delete file with id ${fileId}, status ${response.status}`
+        `${this._TAG_LOG}: delete file success with id ${fileId}, status ${response.status}`
       );
       return true;
     } catch (error) {
-      console.log(`[DELETE_FAILED]: ${error.message}`);
+      console.log(`${this._TAG_LOG_FAIL}: delete file fail, ${error.message}`);
       return false;
     }
   }
